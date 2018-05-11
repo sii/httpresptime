98fd4a3e6edb34b8fb043cc10ccf6df27f185ed5
Simon Ekstrand <simon@beebyte.se>
2018-05-11 23:27:36 +0200

    Cosmetic cleanups.

--- httpresptime.py
+++ httpresptime.py
@@ -14,7 +14,6 @@ Sending requests: .....
 Response times (s): min: 0.0562 max: 0.0647 avg: 0.0607
 """
 
-import sys
 import time
 import argparse
 import datetime
@@ -23,13 +22,13 @@ import socket
 from urllib.parse import urlparse
 ## Disable warnings about not doing SSL verification
 import urllib3
-urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 CHROME_USER_AGENT = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36'
 
 
-def request_headers(headers = {}):
+def request_headers(headers={}):
     """Permanent store for request headers.
 
     Returns a dict of request headers that can be updated for future calls.
@@ -91,8 +90,8 @@ def calc_resp_times(resp_times):
 def display_url_info(url, include_headers=False):
     """Display information about an URL."""
     r = requests.get(url, verify=False, headers=request_headers())
-    print('Input URL: %s' % (url))
-    print('Final URL: %s' % (r.url))
+    print('Input URL: %s' % url)
+    print('Final URL: %s' % r.url)
     print('HTTP status code: %d' % r.status_code)
     print('Response size: %d' % len(r.text))
     print('Number of redirects: %d' % len(r.history))
@@ -123,22 +122,22 @@ def parse_args():
     parser = argparse.ArgumentParser(description='HTTP response time checker..')
     parser.add_argument('-n', '--requests', default=5, type=int, help='number of requests to run')
     parser.add_argument('--no-keepalive', default=True, dest='keepalive', action='store_false',
-            help='disable http keepalive')
+                        help='disable http keepalive')
     parser.add_argument('--single', default=False, dest='single', action='store_true',
-            help='only send a single request, try to avoid cached responses')
+                        help='only send a single request, try to avoid cached responses')
     parser.add_argument('-l', '--loop', default=False, action='store_true',
-            help='loop sending requests forever')
+                        help='loop sending requests forever')
     parser.add_argument('--loop-delay', default=10, type=int, help='delay between requests with -l')
     parser.add_argument('-i', '--info', default=False, action='store_true',
-            help='display http response information')
+                        help='display http response information')
     parser.add_argument('-H', '--display-headers', default=False, dest='headers', action='store_true',
-            help='display http response information headers (only with -i)')
+                        help='display http response information headers (only with -i)')
     parser.add_argument('-p', '--parsable', default=False, action='store_true',
-            help='machine parsable output')
+                        help='machine parsable output')
     parser.add_argument('-r', '--report', default=False, action='store_true',
-            help='report mode, output appropriate for sharing with others')
+                        help='report mode, output appropriate for sharing with others')
     parser.add_argument('--ua-spoof', default=False, action='store_true',
-            help='spoof a Chrome user agent')
+                        help='spoof a Chrome user agent')
     parser.add_argument('url', help='URL to check')
     args = parser.parse_args()
     return args
@@ -185,7 +184,8 @@ def main():
             print('Minimum: %.04f' % (resp['min_time']))
             print('Maximum: %.04f' % (resp['max_time']))
         else:
-            print('Response times (s): min: %.04f max: %.04f avg: %.04f' % (resp['min_time'], resp['max_time'], resp['avg_time']))
+            print('Response times (s): min: %.04f max: %.04f avg: %.04f' % (
+                resp['min_time'], resp['max_time'], resp['avg_time']))
 
 
 if __name__ == '__main__':
